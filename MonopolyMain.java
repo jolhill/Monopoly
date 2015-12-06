ddbaae292248aa55080a8d46e420666426f52e76
jolhill <joshua.hill55@gmail.com>
2015-12-06 16:58:04 -0600

    Added buying houses

--- MonopolyMain.java
+++ MonopolyMain.java
@@ -14,28 +14,39 @@ public class MonopolyMain {
 		Properties [] aBoard = setup.propertiesSetup();
 		Turn turn = new Turn(aPlayers.length);
 		boolean bDone = false;
+		
+		//EACH TIME THIS LOOPS IS A DIFFERENT PLAYER'S TURN
 		while(!bDone){
+			
+			JOptionPane.showMessageDialog(null,(aPlayers[turn.getTurn()].getPlayerName()+ "'s Turn"));
+			
+			//Account Summary
 			System.out.println("\n\n----------------------------" +" "+aPlayers[turn.getTurn()].getPlayerName() + "'s turn " +"---------------------------");
-			System.out.print(aPlayers[turn.getTurn()].statusReport() + aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName()
+			System.out.print("Beginning: "+aPlayers[turn.getTurn()].statusReport() + aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName()
 					+ " and you own the following properties: ");
 			System.out.print(aPlayers[turn.getTurn()].getStrListOfPropsOwned(aBoard));
+			
 			//Rolls dice + appropriate response to roll
 			int iDoublesCount = 0;
 			boolean bDoubles;
+			//Do While Doubles
 			do{
 				bDoubles = false;
-				//Menu.YesNoPrompt(aPlayers[turn.getTurn()].getPlayerName()+"'s roll");
-				JOptionPane.showMessageDialog(null, "Roll");
-				System.out.print(aPlayers[turn.getTurn()].getPlayerName());
+				JOptionPane.showMessageDialog(null,"Roll");
+				
 				int [] aRoll = Randoms.RandomDice();
+				//IF DOUBLES ROLL AGAIN
 				if(aRoll[0]==aRoll[1]){
 					bDoubles = true;
 					iDoublesCount++;
 				}
+				//IF 3 DOUBLES
 				if(iDoublesCount == 3){
 					aPlayers[turn.getTurn()].setBoardPosition(30);
 					break;
 				}
+				//IF IN JAIL
+				//FIX IF TWO ROLLS IN JAIL WILL COUNT AS TWO TURNS SPENT
 				if(aPlayers[turn.getTurn()].getJailStatus()){
 					if(bDoubles){
 						aPlayers[turn.getTurn()].toggleJail();
@@ -44,43 +55,73 @@ public class MonopolyMain {
 						aPlayers[turn.getTurn()].turnInJail();
 					}
 				}
+				//IF NOT IN JAIL
 				else{
-				aPlayers[turn.getTurn()].moveForward(aRoll[2]);
-				System.out.println("You landed on " + aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName() + "!");
-				}
+					aPlayers[turn.getTurn()].moveForward(aRoll[2]);
+					JOptionPane.showMessageDialog(null,("You landed on " + aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName() + "!"));
 				
-				//Checks for ownership of what they landed on
-				aPlayers[turn.getTurn()].getBoardPosition();
-				boolean bOwned = false;
-				//Possibly an issue, but probably fine, if I don't give it an initial value, 
-				//later it says that it may not have been initialized
-				int iOwnerID = 0;
-				for(int i = 0; i < aPlayers.length; i ++){
-					int [] aPropsOwned = aPlayers[i].getOwnedProperties();
-					for(int j = 0; j < aPropsOwned.length; j++){
-						if(aPropsOwned[j] == aPlayers[turn.getTurn()].getBoardPosition()){
-							bOwned = true;
-							iOwnerID = i;
+				
+					//Checks for ownership of what they landed on
+					aPlayers[turn.getTurn()].getBoardPosition();
+					boolean bOwned = false;
+					//Possibly an issue, but probably fine, if I don't give it an initial value, 
+					//later it says that it may not have been initialized
+					int iOwnerID = 0;
+					for(int i = 0; i < aPlayers.length; i ++){
+						int [] aPropsOwned = aPlayers[i].getOwnedProperties();
+						for(int j = 0; j < aPropsOwned.length; j++){
+							if(aPropsOwned[j] == aPlayers[turn.getTurn()].getBoardPosition()){
+								bOwned = true;
+								iOwnerID = i;
+							}
 						}
 					}
-				}
-				if(bOwned){
-					if(iOwnerID != turn.getTurn() ){
-						int amount = 100;
-						turn.transferMoney(amount, turn.getTurn(), iOwnerID, aPlayers);
+					int iLandedOn = aPlayers[turn.getTurn()].getBoardPosition();
+					//Special Places to land on
+					if(iLandedOn == 2 ||iLandedOn == 17 ||iLandedOn == 33){
+						aPlayers = CommunityChest.card(aPlayers, aPlayers[turn.getTurn()].getPlayerID(), aBoard);
 					}
-				}
-				else if(aBoard[aPlayers[turn.getTurn()].getBoardPosition()].isBuyable()){
-					boolean bAnswer = Menu.YesNoPrompt("Would you like to buy " 
-							+ aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName()+ " for $"
-							+aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getiCost() +"?");
-					if(bAnswer){
-						aPlayers[turn.getTurn()].toggleOwnership(aPlayers[turn.getTurn()].getBoardPosition());
-						aPlayers[turn.getTurn()].setMoney(aPlayers[turn.getTurn()].getPlayerMoney()-aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getiCost());
+					else if(iLandedOn == 7 ||iLandedOn == 22 ||iLandedOn == 36){
+						aPlayers = Chance.card(aPlayers, aPlayers[turn.getTurn()].getPlayerID(), aBoard);
 					}
+					else if(iLandedOn == 4){
+						aPlayers[turn.getTurn()].setMoney(aPlayers[turn.getTurn()].getPlayerMoney()-200);
+						JOptionPane.showMessageDialog(null,"You paid $200 for Income Tax!");
+					}
+					else if(iLandedOn == 38){
+						aPlayers[turn.getTurn()].setMoney(aPlayers[turn.getTurn()].getPlayerMoney()-75);
+						JOptionPane.showMessageDialog(null,"You paid $75 for Luxury Tax!");
+					}
+					else if(iLandedOn == 30){
+						aPlayers[turn.getTurn()].setBoardPosition(10);
+						aPlayers[turn.getTurn()].toggleJail();
+						JOptionPane.showMessageDialog(null,"Go Directly to Jail, Do not pass go, Do not Collect 200!");
+					}
+					
+					if(bOwned){
+						if(iOwnerID != turn.getTurn() ){
+							int amount = 100;
+							turn.transferMoney(amount, turn.getTurn(), iOwnerID, aPlayers);
+						}
+					}
+					else if(aBoard[aPlayers[turn.getTurn()].getBoardPosition()].isBuyable()){
+						boolean bAnswer = Menu.YesNoPrompt("Would you like to buy " 
+								+ aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName()+ " for $"
+								+aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getiCost() +"?");
+						if(bAnswer){
+							aPlayers[turn.getTurn()].toggleOwnership(aPlayers[turn.getTurn()].getBoardPosition());
+							aPlayers[turn.getTurn()].setMoney(aPlayers[turn.getTurn()].getPlayerMoney()-aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getiCost());
+						}
+					}
+					aBoard = aPlayers[turn.getTurn()].buyHouses(aBoard);
 				}
 			}while(bDoubles);
+			//END OF TURN SUMMARY
+			System.out.print("End: "+aPlayers[turn.getTurn()].statusReport() + aBoard[aPlayers[turn.getTurn()].getBoardPosition()].getsName()
+					+ " and you own the following properties: ");
+			System.out.print(aPlayers[turn.getTurn()].getStrListOfPropsOwned(aBoard));
 			
+			//SWITCHES TURN TO THE NEXT PLAYER
 			turn.incrementTurn();
 		}
 		
