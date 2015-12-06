0162e9b1f617e9240c22f0b089ce3a1695c6f865
jolhill <joshua.hill55@gmail.com>
2015-12-06 17:01:51 -0600

    JOptionPane Responses to User

--- Randoms.java
+++ Randoms.java
@@ -1,7 +1,7 @@
 /*Random class
  * 
  */
- 
+ import javax.swing.JOptionPane;
 import java.util.Random; 
 public class Randoms {
 	public static int [] RandomDice(){
@@ -9,9 +9,9 @@ public class Randoms {
 		int iDie1 = oRandNbr.nextInt(6)+1;
 		int iDie2 = oRandNbr.nextInt(6)+1;
 		int iMove = iDie1 + iDie2;
-		System.out.println(" rolled a "+iDie1+" and a "+ iDie2+ " and moved " + iMove + " spaces");
+		JOptionPane.showMessageDialog(null,("You rolled a "+iDie1+" and a "+ iDie2+ " and moved " + iMove + " spaces"));
 		if (iDie1 == 1 && iDie2 == 1){
-			System.out.println("Snake Eyes");
+			JOptionPane.showMessageDialog(null,"Snake Eyes");
 		}
 		//roll again
 		int [] aOut = {iDie1,iDie2,iMove}; // array[0..1] ==  each die and array[2] == move amount
